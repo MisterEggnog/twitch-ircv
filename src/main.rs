27206498d6b96e87250703514fc0ee38c8e3a7f2
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2023-02-23 13:25:48 -0800

    Setup message echo based on twitch-irc example

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
+use std::env;
+use twitch_irc::login::StaticLoginCredentials;
+use twitch_irc::TwitchIRCClient;
+use twitch_irc::{ClientConfig, SecureTCPTransport};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let channel = env::args()
+        .nth(1)
+        .expect("Must have channel name as first arg");
+
+    let config = ClientConfig::default();
+    let (mut incoming_messages, client) =
+        TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(config);
+
+    let join_handle = tokio::spawn(async move {
+        while let Some(message) = incoming_messages.recv().await {
+            println!("{:?}", message);
+        }
+    });
+
+    client.join(channel).unwrap();
+
+    join_handle.await.unwrap();
 }
