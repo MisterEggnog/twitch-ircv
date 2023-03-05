b138ebd9813d4bd582a6345bde3ae6f54aa55b70
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2023-03-04 20:27:50 -0800

    Finished privmessage print function test

--- src/lib.rs
+++ src/lib.rs
@@ -13,14 +13,16 @@ pub async fn message_handler(message: ServerMessage, start_time: DateTime<Utc>) 
 
 async fn print_chat_msg<W: Write>(msg: PrivmsgMessage, start_time: DateTime<Utc>, out: &mut W) {
     let time_since_start = msg.server_timestamp.signed_duration_since(start_time);
-    println!(
+    writeln!(
+        out,
         "{:02}:{:02}:{:02} {}: {}",
         time_since_start.num_hours(),
         time_since_start.num_minutes(),
         time_since_start.num_seconds(),
         msg.sender.name,
         msg.message_text
-    );
+    )
+    .expect("Not going to bother to check this lol");
 }
 
 #[tokio::test]
@@ -37,12 +39,22 @@ async fn print_chat_msg_test() {
         name: sender_name.clone(),
         ..Default::default()
     };
-    let message_text = "AAAAAAAAAAAAAAAAAA.".to_string();
+    let message_text_ = "AAAAAAAAAAAAAAAAAA.".to_string();
 
     let message = PrivmsgMessage {
         sender,
         server_timestamp: message_time,
-        message_text,
+        message_text: message_text_.clone(),
         ..Default::default()
     };
+
+    let mut output = vec![];
+
+    print_chat_msg(message, start_time, &mut output).await;
+    assert_eq!(
+        output,
+        format!("11:11:11 {sender_name}: {message_text_}\n").into_bytes(),
+        "\n{}",
+        String::from_utf8_lossy(&output)
+    );
 }
