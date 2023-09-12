a49b377d84f609ca72194ee1b126d717716a3691
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2023-09-11 22:38:44 -0700

    Wrote function to parse badges

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use chrono::prelude::*;
 use colored::Colorize;
 use std::io::prelude::*;
+use twitch_irc::message::Badge;
 use twitch_irc::message::PrivmsgMessage;
 use twitch_irc::message::ServerMessage;
 
@@ -13,6 +14,23 @@ pub async fn message_handler<W: Write>(
         ServerMessage::Privmsg(msg) => print_chat_msg(msg, start_time, out).await,
         _ => (),
     }
+}
+
+async fn parse_badges(badges: &[Badge]) -> (Option<&'static str>, Option<i64>) {
+    let mut channel_status = None;
+    let mut sub_badge_month = None;
+    for badge in badges {
+        match badge.name.as_str() {
+            "broadcaster" => channel_status = Some("📹"),
+            "moderator" => channel_status = Some("🗡️"),
+            "vip" => channel_status = Some("💎"),
+            "subscriber" => sub_badge_month = badge.version.parse().ok(),
+            // TODO "partner"
+            // TODO "staff"
+            _ => (),
+        }
+    }
+    (channel_status, sub_badge_month)
 }
 
 async fn print_chat_msg<W: Write>(msg: PrivmsgMessage, start_time: DateTime<Utc>, out: &mut W) {
@@ -66,4 +84,36 @@ async fn print_chat_msg_test() {
         "\n{}",
         String::from_utf8_lossy(&output)
     );
+}
+
+#[tokio::test]
+async fn test_parse_badges() {
+    let valid_strings = ["broadcaster", "moderator", "vip"];
+    for s in valid_strings {
+        let test_badges = [
+            Badge {
+                name: "beep".to_string(),
+                version: "6".to_string(),
+            },
+            Badge {
+                name: s.to_string(),
+                version: "1".to_string(),
+            },
+        ];
+        let (permission_badge, _) = parse_badges(&test_badges).await;
+        assert!(permission_badge.is_some());
+    }
+
+    let test_badges = [
+        Badge {
+            name: "adsad".to_string(),
+            version: "202".to_string(),
+        },
+        Badge {
+            name: "subscriber".to_string(),
+            version: "90210".to_string(),
+        },
+    ];
+    let (_, sub_badge) = parse_badges(&test_badges).await;
+    assert_eq!(sub_badge, Some(90210));
 }
