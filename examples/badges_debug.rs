ded33faccbc316517199d9fe97ed86d8f22d6308
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2024-03-05 15:21:33 -0800

    Added helper program to print badge debug info

---
+++ examples/badges_debug.rs
@@ -0,0 +1,29 @@
+use std::env;
+use std::error::Error;
+use twitch_irc::login::StaticLoginCredentials;
+use twitch_irc::message::ServerMessage;
+use twitch_irc::TwitchIRCClient;
+use twitch_irc::{ClientConfig, SecureTCPTransport};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let channel = env::args()
+        .nth(1)
+        .expect("Must have channel name as first arg");
+
+    let config = ClientConfig::default();
+    let (mut incoming_messages, client) =
+        TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(config);
+
+    let join_handle = tokio::spawn(async move {
+        while let Some(message) = incoming_messages.recv().await {
+            if let ServerMessage::Privmsg(message) = message {
+                println!("{} {:?}", message.sender.name, message.badges);
+            }
+        }
+    });
+
+    client.join(channel)?;
+    join_handle.await?;
+    Ok(())
+}
