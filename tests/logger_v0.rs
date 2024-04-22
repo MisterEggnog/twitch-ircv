d401c59ed4caee3cefdd059916d9c5ccbcda0407
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2024-04-21 17:21:22 -0700

    Write logging results 2 inmemory buffer 4 testing

--- tests/logger_v0.rs
+++ tests/logger_v0.rs
@@ -1,7 +1,12 @@
+use std::error::Error;
 use std::fs::File;
-use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
+use std::io::Cursor;
+
+use twitch_irc::message::{IRCMessage, ServerMessage};
+
+use twitch_chat_logger::logging::log_v0;
 
 fn valid_irc(s: &str) -> bool {
     s.contains("PRIVMSG")
@@ -10,8 +15,8 @@ fn valid_irc(s: &str) -> bool {
         || s.contains("CLEARMSG")
 }
 
-#[test]
-fn test_no_ping() -> io::Result<()> {
+#[tokio::test]
+async fn test_no_ping() -> Result<(), Box<dyn Error>> {
     let f = File::open("tests/irc_data_no_ping")?;
     let f = BufReader::new(f);
     let irc_lines: Vec<String> = f.lines().map(|s| s.unwrap()).collect();
@@ -21,6 +26,13 @@ fn test_no_ping() -> io::Result<()> {
         .filter(|&s| valid_irc(s))
         .map(|s| s.as_str())
         .collect();
+
+    let mut buff = Cursor::new(vec![]);
+    for line in irc_lines {
+        let msg = IRCMessage::parse(&line)?;
+        let msg = ServerMessage::try_from(msg)?;
+        log_v0(msg, &mut buff).await;
+    }
 
     Ok(())
 }
