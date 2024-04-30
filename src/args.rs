7be36baf0d4beb8ff01c9c45e607af22a4e66477
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2024-04-29 20:24:22 -0700

    Ran cargo fmt

--- src/args.rs
+++ src/args.rs
@@ -1,12 +1,13 @@
 use argh::FromArgs;
 
-/// TODO
+/// Pretty print the live chat of a twitch channel.
+/// Also offers support for logging (most) of the irc messages posted in chat.
 #[derive(FromArgs)]
 pub struct Args {
-	#[argh(positional)]
-	pub channel_name: String,
+    #[argh(positional)]
+    pub channel_name: String,
 
-	/// file to write irc log to.
-	#[argh(option, short = 'o')]
-	pub log_file: Option<String>,
+    /// file to write irc log to.
+    #[argh(option, short = 'o')]
+    pub log_file: Option<String>,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 //! This is for testing purposes, please use the binary.
+pub mod args;
 pub mod badges;
 pub mod chat_logger;
 pub mod logging;
-pub mod args;
