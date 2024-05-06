da6440e940650a3c6ee5b477504ea98bc7a9fc23
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2024-05-06 13:43:15 -0700

    Moved irc setup to its own function

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,13 @@
-use std::io::stdout;
-use twitch_irc::login::StaticLoginCredentials;
-use twitch_irc::TwitchIRCClient;
-use twitch_irc::{ClientConfig, SecureTCPTransport};
-
 mod args;
 mod badges;
 mod chat_logger;
 mod logging;
 mod setup;
-use chat_logger::*;
 
 #[tokio::main]
 async fn main() {
     let args: args::Args = argh::from_env();
     let channel = args.channel_name;
 
-    let startup_time = chrono::Utc::now();
-    println!("Logging started at {}", startup_time);
-
-    let config = ClientConfig::default();
-    let (mut incoming_messages, client) =
-        TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(config);
-
-    let mut stdout = stdout();
-    let join_handle = tokio::spawn(async move {
-        while let Some(message) = incoming_messages.recv().await {
-            if !message_handler(message, startup_time, &mut stdout)
-                .await
-                .expect("Failed to write message")
-            {
-                break;
-            }
-        }
-    });
-
-    client.join(channel).unwrap();
-
-    join_handle.await.unwrap();
+    setup::setup_irc_client(channel, |_| {}).await;
 }
