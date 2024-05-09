faaaa33ad0be21846591d37acc539faf8fbf7739
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2024-05-09 11:24:38 -0700

    Changed func signature for twitch setup

--- src/setup.rs
+++ src/setup.rs
@@ -1,7 +1,8 @@
+use tokio::sync::mpsc::UnboundedReceiver;
 use twitch_irc::message::ServerMessage;
+use twitch_irc::TwitchIRCClient;
 
-fn setup_irc_client<F>(channel: &str, message_dest: F)
-where
-    F: Fn(ServerMessage),
-{
+/// Simplified version of TwitchIRCClient::new with default config
+fn setup_irc_client(channel: &str) -> UnboundedReceiver<ServerMessage> {
+    todo!()
 }
