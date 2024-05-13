5191cbb63aca61fd88f96ecd513542be0aeda986
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2024-05-12 18:08:41 -0700

    Note env variables for cli color
    Argh mangles the formatting of this.
    I would like the formatting to be like,
    ```
    ...
    Note:
    Color can be prohibited with the enviromental variable NO_COLOR.
    Color can be forced using the enviromental variable CLICOLOR_FORCE.
    ```
    
    Right now it just does this
    `... Note: Color can be prohibited with the enviromental variable NO_COLOR. Color can be forced using the enviromental variable CLICOLOR_FORCE.`
    This seems to be a known issue with the argh library, there
    is a pull request for this but I don't know when this will
    enter crates.io.
    
    Rather unfortunately at the end of the comments for the issue
    that discusses this, the author states they have switched
    to clap.
    I hope I won't have to do the same.

--- src/args.rs
+++ src/args.rs
@@ -2,6 +2,10 @@ use argh::FromArgs;
 
 /// Pretty print the live chat of a twitch channel.
 /// Also offers support for logging (most) of the irc messages posted in chat.
+///
+/// Note:
+/// Color can be prohibited with the enviromental variable NO_COLOR.
+/// Color can be forced using the enviromental variable CLICOLOR_FORCE.
 #[derive(FromArgs)]
 pub struct Args {
     #[argh(positional)]
