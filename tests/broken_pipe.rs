6551accd6de438c0a27c367746c64293eeeeeb31
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2025-09-14 10:24:51 -0700

    removed warning in broken_pipe test

--- tests/broken_pipe.rs
+++ tests/broken_pipe.rs
@@ -18,6 +18,7 @@ impl Write for PanicsBrokenPipe {
 }
 
 #[tokio::test]
+#[allow(unused_must_use)]
 async fn cleanly_exits_with_broken_pipe() -> io::Result<()> {
     let fake_stdin = File::open("tests/irc_data_no_ping")?;
     let args = Args {
@@ -25,5 +26,6 @@ async fn cleanly_exits_with_broken_pipe() -> io::Result<()> {
         ..Default::default()
     };
 
+    // We do not need to handle this result as it will bubble up
     init(args, fake_stdin, PanicsBrokenPipe).await
 }
