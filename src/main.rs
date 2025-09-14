1801ec45347d5e9026272c9544bfd044e85ebaad
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2025-09-14 09:40:20 -0700

    Revert "main handles broken pipes"
    This reverts commit 9e7b6875387a37839350d3f5714c431af3c78f03.

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,10 @@ mod logging;
 mod pretty_print;
 mod setup;
 
-use std::io;
+use std::io::{stdin, stdout};
 
 #[tokio::main]
-async fn main() -> io::Result<()> {
+async fn main() {
     let args: args::Args = argh::from_env();
-    let res = setup::init(args, io::stdin(), io::stdout()).await;
-    if let Err(e) = res {
-        if e.kind() == io::ErrorKind::BrokenPipe {
-            Ok(())
-        } else {
-            Err(e)
-        }
-    } else {
-        Ok(())
-    }
+    setup::init(args, stdin(), stdout()).await;
 }
