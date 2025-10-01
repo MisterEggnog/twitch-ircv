eb2ec255c18a5bf716564e0bba3bd050538d5d9d
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2025-10-01 11:48:23 -0700

    Implemented clap arg parsing
    I likely won't be using this, the result is not impressive to me

--- src/args.rs
+++ src/args.rs
@@ -1,4 +1,5 @@
 use argh::FromArgs;
+use clap::Parser;
 use std::path::PathBuf;
 
 /// Pretty print the live chat of a twitch channel.
@@ -11,26 +12,30 @@ use std::path::PathBuf;
 /// Warning:
 /// print-raw-irc does not give consistent output, you cannot feed the output
 /// of this into `from-stdin`, & have the same result.
-#[derive(FromArgs, Default)]
+#[derive(FromArgs, Default, Parser)]
 pub struct Args {
     #[argh(positional)]
     pub channel_name: String,
 
     /// file to write irc log to.
     #[argh(option, short = 'o')]
+    #[arg(short = 'o', long)]
     pub log_file: Option<PathBuf>,
 
     /// append to log file, not overwrite it.
     #[argh(switch, short = 'a')]
+    #[arg(short = 'a', long)]
     pub append: bool,
 
     /// don't connect to a twitch irc channel, read raw irc from stdin.
     /// Note that you are still required to include a channel_name, it will be
     /// ignored.
     #[argh(switch)]
+    #[arg(long)]
     pub from_stdin: bool,
 
     /// output the raw irc
     #[argh(switch)]
+    #[arg(long)]
     pub print_raw_irc: bool,
 }
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,12 @@ mod logging;
 mod pretty_print;
 mod setup;
 
+use clap::Parser;
 use std::io;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let args: args::Args = argh::from_env();
+    //let args: args::Args = argh::from_env();
+    let args = args::Args::parse();
     setup::init(args, io::stdin(), io::stdout()).await
 }
