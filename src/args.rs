b0f7bcfc6bde52c17789838253a72c10d95d3649
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2025-10-07 10:59:06 -0700

    Removed argh dependency

--- src/args.rs
+++ src/args.rs
@@ -1,4 +1,3 @@
-use argh::FromArgs;
 use clap::Parser;
 use std::path::PathBuf;
 
@@ -12,7 +11,7 @@ use std::path::PathBuf;
 /// Warning:
 /// print-raw-irc does not give consistent output, you cannot feed the output
 /// of this into `from-stdin`, & have the same result.
-#[derive(FromArgs, Default, Parser)]
+#[derive(Default, Parser)]
 #[command(long_about = concat!(
 "Pretty print twitch chat\n",
 "\n",
@@ -27,28 +26,23 @@ use std::path::PathBuf;
 "  • CLICOLOR_FORCE: Forces color output.",
 ))]
 pub struct Args {
-    #[argh(positional)]
     pub channel_name: String,
 
     /// file to write irc log to.
-    #[argh(option, short = 'o')]
     #[arg(short = 'o', long)]
     pub log_file: Option<PathBuf>,
 
     /// append to log file, not overwrite it.
-    #[argh(switch, short = 'a')]
     #[arg(short = 'a', long)]
     pub append: bool,
 
     /// don't connect to a twitch irc channel, read raw irc from stdin.
     /// Note that you are still required to include a channel_name, it will be
     /// ignored.
-    #[argh(switch)]
     #[arg(long)]
     pub from_stdin: bool,
 
     /// output the raw irc
-    #[argh(switch)]
     #[arg(long)]
     pub print_raw_irc: bool,
 }
