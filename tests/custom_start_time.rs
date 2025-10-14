fc1e7e95a1c56c40bed660da2a402857ef42a984
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2025-10-13 22:02:29 -0700

    Started test for custom timestamp

---
+++ tests/custom_start_time.rs
@@ -0,0 +1,7 @@
+// Parse's datetime from the example produced by
+// date +%s.%N
+// To do this run
+// TWITCH_IRCV_START_TIME={seconds}.{nanoseconds}
+// These are seconds since UNIX timestamp
+//
+// This is probably parsed using DateTime::from_timestamp
