c3e884c49a37ec889bd7f0279974b04c82a22167
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2026-07-26 20:16:06 -0700

    Replaced init_no_error_handling unwraps wth expect

--- src/setup.rs
+++ src/setup.rs
@@ -40,12 +40,15 @@ where
     if args.from_stdin {
         let (handle, recv) = filein_channel_task_create(stdin);
         let (handle_res, stdout_result) = tokio::join!(handle, init_with_input(args, recv, stdout));
-        handle_res.unwrap();
+        handle_res.expect("filein task should've closed by this point");
         stdout_result
     } else {
         let (incoming_messages, client) = build_irc_client();
 
-        client.join(args.channel_name.clone()).unwrap();
+        client
+            .join(args.channel_name.clone())
+            .expect("Channel name is an invalid format");
+        // TODO More gracefuly handle this
 
         init_with_input(args, incoming_messages, stdout).await
     }
