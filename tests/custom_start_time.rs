0a41c15da102bbb07710e431d58bd7c0afde9bad
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2026-07-29 13:11:53 -0700

    Removed unwraps in custom_start_time file

--- tests/custom_start_time.rs
+++ tests/custom_start_time.rs
@@ -20,16 +20,16 @@ fn custom_datetime_env_variable() -> io::Result<()> {
     let data: Vec<String> = result
         .stdout
         .lines()
-        .map(|l| l.unwrap())
+        .map(|l| l.expect("input is valid utf8"))
         .map(|l| l.trim().to_string())
         .collect();
-    let err_str = String::from_utf8(result.stderr).unwrap();
+    let err_str = String::from_utf8(result.stderr).expect("input is valid utf8");
 
     assert_eq!(result.status.code(), Some(0), "{}", err_str);
 
     let mut data = data.into_iter();
     // Drop first line
-    let _ = data.next().unwrap();
+    let _ = data.next().expect("Should be @ least 1 line of text");
     let message_prefixs = ["00:00:00", "00:00:01", "00:00:02"];
     for (result, expected_start) in data.zip(message_prefixs) {
         assert!(
